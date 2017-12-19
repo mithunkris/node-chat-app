1a09f3fe465f272fdfe96eb3df4bee6fe9a2ad1b
Mithun Krishnan <mithun.krishnan@o9solutions.com>
2017-12-19 07:51:04 +0530

    New utils message

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 var publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
@@ -16,31 +17,19 @@ io.on('connection',(socket)=>{
     console.log('User Disconnected');
   });
 
-  socket.emit('newMessage',{
-    from:'Admin',
-    text:'Welcome to the chat room',
-    createdAt:new Date().getTime()
-  })
-  socket.broadcast.emit('newMessage',{
-    from:'Admin',
-    text:'New User joined',
-    createdAt:new Date().getTime()
-  });
+  socket.emit('newMessage',generateMessage('Admin','Welcome to te chat app'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined'));
 
   socket.on('createMessage',(message)=>{
     console.log('New message Created',message);
-    // io.emit('newMessage',{
-    //   from:message.from,
-    //   text:message.text,
-    //   createdAt:new Date().getTime()
-    // })
+     io.emit('newMessage',generateMessage(message.from,message.text));
+    })
     // socket.broadcast.emit('newMessage',{
     //   from:message.from,
     //   text:message.text,
     //   createdAt:new Date().getTime()
     // });
-  });
-});
+ });
 
 
 app.use(express.static(publicPath));
