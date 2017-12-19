cd8f5fe799ec23474d2d92617c1ab888651ea20b
Mithun Krishnan <mithun.krishnan@o9solutions.com>
2017-12-19 06:38:56 +0530

    Broadcast event

--- server/server.js
+++ server/server.js
@@ -16,13 +16,29 @@ io.on('connection',(socket)=>{
     console.log('User Disconnected');
   });
 
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat room',
+    createdAt:new Date().getTime()
+  })
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New User joined',
+    createdAt:new Date().getTime()
+  });
+
   socket.on('createMessage',(message)=>{
     console.log('New message Created',message);
-    io.emit('newMessage',{
-      from:message.from,
-      text:message.text,
-      createdAt:new Date().getTime()
-    })
+    // io.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // })
+    // socket.broadcast.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // });
   });
 });
 
